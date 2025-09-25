a3dda8b607530d4475e9d40c35c7b18ccec27106
kaio <kaiovinicios05@gmail.com>
2025-09-25 20:13:21 -0300

    build att

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
+
+// Copiar HTML para dist
+function html() {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+// Compilar SCSS para CSS minificado
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-
+// Copiar imagens
 function images() {
     return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-function html() {
-    return gulp.src('./*.html')
-    .pipe(gulp.dest('./dist'));
+// Concatenar e minificar JS
+function scripts() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 }
 
-exports.default =gulp.parallel(styles, images, html);
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./*.html', gulp.parallel(html));
-};
+// Task build
+const build = gulp.parallel(html, styles, images, scripts);
+
+exports.html = html;
+exports.styles = styles;
+exports.images = images;
+exports.scripts = scripts;
+exports.build = build;
+exports.default = build;
